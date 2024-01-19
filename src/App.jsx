33012acaeb9c1b35aebd4c464448a0f80171e0e9
fadrian06 <franyeradriansanchez@gmail.com>
2024-01-19 14:32:04 -0400

    Don't define components inside of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 const Button = props => <button onClick={props.handleClick}>{props.text}</button>
+const Display = props => <div>{props.value}</div>
 
 const App = () => {
   const [value, setValue] = useState(10)
@@ -9,7 +10,7 @@ const App = () => {
 
   return (
     <div>
-      {value}
+      <Display value={value} />
       <Button handleClick={setToValue(1000)} text='thousand' />
       <Button handleClick={setToValue(0)} text='reset' />
       <Button handleClick={setToValue(value + 1)} text='increment' />
