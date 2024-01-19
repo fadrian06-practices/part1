648f4912d0c095f1972380f25c4335575d7f5ee0
fadrian06 <franyeradriansanchez@gmail.com>
2024-01-19 01:06:06 -0400

    Complex state, step 3

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,11 @@ const App = () => {
   const [clicks, setClicks] = useState({ left: 0, right: 0 })
 
   const handleLeftClick = () => {
-    const newClicks = {
-      left: clicks.left + 1,
-      right: clicks.right
-    }
-
-    setClicks(newClicks)
+    setClicks({ ...clicks, left: clicks.left + 1 })
   }
 
   const handleRightClick = () => {
-    const newClicks = {
-      left: clicks.left,
-      right: clicks.right + 1
-    }
-
-    setClicks(newClicks)
+    setClicks({ ...clicks, right: clicks.right + 1 })
   }
 
   return (
