5f91e13ff516a14ed52afcdf69a74f0e3282fb51
fadrian06 <franyeradriansanchez@gmail.com>
2024-01-19 01:01:36 -0400

    Complex state, step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from 'react'
+
 const App = () => {
-  const friends = ['Peter', 'Maya']
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
   return (
     <div>
-      <p>{friends}</p>
+      {left}
+      <button onClick={() => setLeft(left + 1)}>left</button>
+      <button onClick={() => setRight(right + 1)}>right</button>
+      {right}
     </div>
   )
 }
