a09671f667b9d7c9eb02a7a2afde4c3fbe624571
fadrian06 <franyeradriansanchez@gmail.com>
2024-01-19 14:29:20 -0400

    A function that returns other function

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { useState } from 'react'
 const App = () => {
   const [value, setValue] = useState(10)
 
-  const handleClick = () => setValue(0)
+  const setToValue = newValue => () => setValue(newValue)
 
   return (
     <div>
       {value}
-      <button onClick={handleClick}>button</button>
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   )
 }
